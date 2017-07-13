c877e7721f19cb83bca6520facfcda6b17b97629
Ryan McCarty <rmac_whf@yahoo.com>
2017-07-13 15:27:34 -0500

    first commit

---
+++ Calculator.Tests/Calculator_CalculatorShould.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorShould
+    {
+        private Calculator _calculator;
+
+        public CalculatorShould()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void AddTwoIntegers()
+        {
+            // Given this input to the method
+            int sum = _calculator.Add(54, 29);
+            Console.WriteLine(sum);
+            // We are asserting that the output should be this
+            Assert.Equal(sum, 83);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+        public class Calculator{
+        public int Add(int sum1, int sum2){
+            return sum1 + sum2;
+        }
+        
+    }
+}
